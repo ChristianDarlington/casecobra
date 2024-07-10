12d980a1ae2e25e669a39b0abbef9ddfe84b0085
Christian Darlington <cdarlington@clearhealthsolution.com>
2024-07-10 17:22:00 -0400

    added some auth and working on customer tests

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Phone from "@/components/Phone";
+import { Icons } from "@/components/Icons"
 import { Check, Star } from "lucide-react";
 
 export default function Home() {
@@ -62,12 +63,12 @@ export default function Home() {
 
                     <div className="flex flex-col justify-between items-center sm:item-start">
                       <div className="flex gap-0.5">
-                        <Star className="h-4 w-4  fill-green-600"/>
-                        <Star className="h-4 w-4 fill-green-600"/>
-                        <Star className="h-4 w-4 fill-green-600"/>
-                        <Star className="h-4 w-4 fill-green-600"/>
-                        <Star className="h-4 w-4 fill-green-600"/>
-                      </div>
+                        <Star className="h-4 w-4 fill-green-600 text-green-500"/>
+                        <Star className="h-4 w-4 fill-green-600 text-green-500"/>
+                        <Star className="h-4 w-4 fill-green-600 text-green-500"/>
+                        <Star className="h-4 w-4 fill-green-600 text-green-500"/>
+                        <Star className="h-4 w-4 fill-green-600 text-green-500"/>
+                      </div> 
 
                       <p>
                         <span className="font-semibold">1,250</span> 
@@ -86,6 +87,54 @@ export default function Home() {
               -top-20 select-none hidden sm:block lg:hidden xl:block"/>
               <img src="/line.png" alt="" className="absolute w-20 -left-6 -bottom-6 select-none"/>
               <Phone className='w-64' imgSrc='/testimonials/1.jpg'/>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+
+      {/* value proposition section */}
+      <section className="bg-slate-100 py-4">
+        <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
+          <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+            <h2 className="order-1 mt-2 tracking-tight text-center text-balance 
+            !leading-tight font-bold text-5xl md:text-6xl text-gray-900">What our <span className="relative 
+            px-2">customers <Icons.Underline className='hidden sm:block pointer-events-none absolute 
+            inset-x-0 -bottom-6 text-green-500'/></span> {" "} say
+            </h2>
+            <img src="/snake-2.png" alt="" className="w-24 order-0 lg:order-2"/>
+          </div>
+
+          <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0
+          lg:max-w-none lg:grid-cols-2 gap-y-16">
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <Star className="h-5 w-5 text-green-500 fill-green-600"/>
+                <Star className="h-5 w-5 text-green-500 fill-green-600"/>
+                <Star className="h-5 w-5 text-green-500 fill-green-600"/>
+                <Star className="h-5 w-5 text-green-500 fill-green-600"/>
+                <Star className="h-5 w-5 text-green-500 fill-green-600"/>
+              </div>
+              <div className="text-large leading-8">
+                <p>
+                  "The case feels durable and I even got a compliment on the design.
+                  Had the case for two and a half months now and <span className="p-0.5 bg-slate-800 text-white">the image is 
+                  super clear
+                  </span>
+                  , on the case I had before, the image started fading into yellow-ish color
+                  after a couple weeks. Love it."
+                </p>
+              </div>
+              <div className="flex gap-4 mt-2">
+                <img src="/users/user-1.png" alt="user" className="rounded-full h-12 w-12 object-cover"/>
+              
+              <div className="flex flex-col">
+                <p className="font-semibold">Hunter</p>
+                <div className="flex gap-1.5 items-center text-zinc-600">
+                  <Check className="h-4 w-4 stroke-[3px] text-green-600"/>
+                  <p className="text-sm">Verified Purchase</p>
+                </div>
+              </div>
+              </div>
             </div>
           </div>
         </MaxWidthWrapper>
