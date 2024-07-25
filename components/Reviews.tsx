893ff9cc0ac9717828e3f0f31b3cac9490066258
Christian Darlington <cdarlington@clearhealthsolution.com>
2024-07-25 09:35:16 -0400

    working on the image uploader along with finishing the steps ui

--- components/Reviews.tsx
+++ components/Reviews.tsx
@@ -56,7 +56,7 @@ function ReviewColumn({
       resizeObserver.disconnect()
     }
   }, [])
-  
+
   return (
     <div
       ref={columnRef}
@@ -145,6 +145,12 @@ function ReviewGrid() {
           />
         </>
       ) : null}
+      <div className="pointer-events-none absolute inset-x-0 top-0 h-32
+      bg-gradient-to-b from-slate-100" 
+      />
+      <div className="pointer-events-none absolute inset-x-0 bottom-0 h-32
+      bg-gradient-to-t from-slate-100" 
+      />
     </div>
   );
 }
